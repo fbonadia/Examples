811352655d5605f358d02cc8c865e3c58761d9d6
fbonadia <fabiobonadia@gmail.com>
2021-05-08 21:36:19 -0300

    more examples js

---
+++ html_css_javasc/js/index.js
@@ -0,0 +1,12 @@
+function start() {
+  const content = {}
+  content.searchTerm = askAndReturnSerchTerm()
+
+  function askAndReturnSerchTerm() {
+    return 'TERM EXAMPLE'
+  }
+
+  console.log(content)
+}
+
+start()
